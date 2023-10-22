5414b7e2917f441aef23f52ecec5b66664b93b40
Kyle Boss <kyle@kyleboss.com>
2023-10-22 15:43:35 -0700

    Update nodeVersion

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,6 +3,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
+  nodeVersion: '18.17.1',
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID!,
