ace892ba063ac658bb5fe8edd95fcaba6acac45f
Kyle Boss <kyle@kyleboss.com>
2023-10-22 15:46:45 -0700

    Update ssgName

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,7 +3,8 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
-  nodeVersion: '18.17.1',
+  nodeVersion: '18',
+  ssgName: 'nextjs',
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID!,
